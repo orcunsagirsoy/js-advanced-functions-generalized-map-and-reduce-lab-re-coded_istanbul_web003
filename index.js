4b91f83d37f3351a07e0affe504d01033053c734
orcunsagirsoy <orcunsagirsoy@gmail.com>
2020-10-30 02:49:56 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Add your functions here
+function map (array, funcy) {
+    let newArray = [];
+
+    for (let i = 0; i < array.length; i++)
+    {
+        newArray.push(funcy(array[i]));
+    }
+
+    return newArray;
+}
+
+function reduce (array, funcy, start) {
+    let tmp;
+
+    if(start) {
+        tmp = start;
+        for (let i = 0; i < array.length; i++) {
+            tmp = funcy(array[i], tmp);
+        }
+    }
+
+    else {
+        tmp = array[0];
+        for (let i = 1; i < array.length; i++) {
+            tmp = funcy(array[i], tmp);
+        }
+    }
+    return tmp;
+}
